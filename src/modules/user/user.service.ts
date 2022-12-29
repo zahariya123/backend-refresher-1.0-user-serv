47dd905c57ca92baf00a94b157fd8d536e9cd477
unknown <zahariya97@gmail.com>
2022-12-29 10:28:11 +0530

    user service commited

---
+++ src/modules/user/user.service.ts
@@ -0,0 +1,9 @@
+/* eslint-disable prettier/prettier */
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class UserService {
+  getHello(): string {
+    return 'Hello World!';
+  }
+}
