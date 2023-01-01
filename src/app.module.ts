b69c08fdd773a9c099d4388f28f6361c60fa6151
unknown <zahariya97@gmail.com>
2023-01-01 11:02:25 +0530

    added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,26 @@
+/* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UserModule } from './user/user.module';
+import { UserModule } from './modules/user/user.module';
+import { User } from './submodules/backend-refresher-1.0-entities/src/entities/user.entity';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: '12345',
+      database: 'backend-socialmedia',
+      entities: [ User ],
+      synchronize: true,
+    }),
+
+    UserModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,13 +1,22 @@
 /* eslint-disable prettier/prettier */
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { UserDto } from 'src/submodules/backend-refresher-1.0-dtos/src/dtos/user.dto';
 import { UserService } from './user.service';
 
-@Controller()
+@Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
-
-  @Get()
-  getHello(): string {
-    return this.userService.getHello();
+  
+  @Post()
+  async createUser(@Body()user: UserDto){     // dtos -> data transfer object
+     try{
+  //     let createdUser = await this.userService.createUser(user);
+    //   return createdUser;
+    console.log("control is here",user);
+     }
+     catch(err){
+       console.log(err);
+       return err;
+     } 
   }
 }
